fcb407d0c0479544177cd62bef22f7bde10a5c5a
WilliamSosa895 <zs23004708@estudiantes.uv.mx>
2025-04-28 17:10:04 -0600

    ssl

--- database/config/config.js
+++ database/config/config.js
@@ -2,15 +2,21 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-   url: process.env.DEV_DATABASE_URL,
-   dialect: 'postgres',
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'postgres',
   },
   test: {
-   url: process.env.TEST_DATABASE_URL,
-   dialect: 'postgres',
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'postgres',
   },
   production: {
-   url: process.env.DATABASE_URL,
-   dialect: 'postgres',
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 }
