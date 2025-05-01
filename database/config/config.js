2dad6dc1741a6a777d2b9bc5e27e143596605070
WilliamSosa895 <zs23004708@estudiantes.uv.mx>
2025-05-01 16:18:09 -0600

    ssl

--- database/config/config.js
+++ database/config/config.js
@@ -1,18 +1,15 @@
 require('dotenv').config()
 
-const sslConfig = {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // A veces necesario si es un certificado autofirmado
-    },
-  },
-};
-
 module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
@@ -21,6 +18,5 @@ module.exports = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    ...sslConfig
   },
 }
